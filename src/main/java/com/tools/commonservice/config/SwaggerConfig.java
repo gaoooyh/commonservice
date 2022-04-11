f50171bad44972fb3081c468b469495b85806f74
gaoyh01 <gaoyh01@rd.netease.com>
2022-04-11 15:57:11 +0800

    whitefilter add swagger ui

--- src/main/java/com/tools/commonservice/config/SwaggerConfig.java
+++ src/main/java/com/tools/commonservice/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.tools.commonservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,7 +13,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer{
 
     @Bean
     public Docket api() {
@@ -31,5 +33,17 @@ public class SwaggerConfig {
                 .license("The Apache License")
                 .licenseUrl("https://www.apache.org/")
                 .build();
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
     }
 }
