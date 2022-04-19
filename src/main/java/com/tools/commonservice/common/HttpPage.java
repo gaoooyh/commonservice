c7bb0183c73c8140fae9800731f6392eb5282111
gaoyh01 <gaoyh01@rd.netease.com>
2022-04-19 14:59:47 +0800

    page demo

--- src/main/java/com/tools/commonservice/common/HttpPage.java
+++ src/main/java/com/tools/commonservice/common/HttpPage.java
@@ -1,2 +1,19 @@
-package com.tools.commonservice.common;public class HttpPage {
+package com.tools.commonservice.common;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+/**
+ * Annotation @Accessors(chain = true) Usage:
+ * page.setPage(10).setTotal(100).setPageSize(30);
+ */
+@Accessors(chain = true)
+@Data
+public class HttpPage<T> {
+    private int total;
+    private int page;
+    private int pageSize;
+    private List<T> data;
 }
